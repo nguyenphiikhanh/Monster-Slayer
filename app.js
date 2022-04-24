a49f0f595e887e614c852f9d01ac8ba7a7bda8a0
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2022-04-25 00:17:27 +0700

    attack ,win & lose alert

--- app.js
+++ app.js
@@ -5,11 +5,57 @@ new Vue({
         monsterHealth: 100,
         gameIsRunning: false,
     },
-    methods:{
-        startNewGame(){
+    methods: {
+        startNewGame() {
             this.gameIsRunning = true;
             this.chibiHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack() {
+            //monster
+            var maxDamage = 10;
+            var minDamage = 4;
+            var damage = Math.max(Math.floor(Math.random() * maxDamage + 1, minDamage));
+            this.monsterHealth -= damage;
+
+
+            //chibi
+            // maxDamage = 20;
+            // minDamage = 8;
+            var damage = Math.max(Math.floor(Math.random() * maxDamage + 1, minDamage));
+            this.chibiHealth -= damage;
+
+
+            if (this.chibiHealth <= 0) { //monster win
+                this,chibiHealth = 0;
+                Swal.fire(
+                    'You lose the Monster!',
+                    "Don't give up! Play again ♥",
+                    'warning'
+                )
+                this.gameIsRunning = false
+                return;
+            }
+            if (this.monsterHealth <= 0) { // chibi win
+                this.monsterHealth = 0;
+                Swal.fire({
+                    title: 'Configuration!',
+                    text:"You've won the Monster!",
+                    icon: 'success',
+                })
+                this.gameIsRunning = false
+                return;
+            }
+
+        },
+        specialAttack() {
+
+        },
+        heal() {
+
+        },
+        giveUp() {
+
         }
     }
 })
