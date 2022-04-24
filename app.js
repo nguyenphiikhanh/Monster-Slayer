f3cd21c4217d9cfcb690e050d20fc92514bb7467
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2022-04-24 23:29:36 +0700

    setup HP when game is started

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ new Vue({
         chibiHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+    },
+    methods:{
+        startNewGame(){
+            this.gameIsRunning = true;
+            this.chibiHealth = 100;
+            this.monsterHealth = 100;
+        }
     }
 })
